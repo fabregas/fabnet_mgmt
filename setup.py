5eaa56b841e5d4680541d7310a430dbe83972cb2
fabregas <kksstt@gmail.com>
2014-05-02 18:11:21 +0300

    append release management stuffw

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from setuptools import setup
+from setup_routines import *
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if __name__ == '__main__':
+    try:
+        install_submodule('https://github.com/fabregas/fabnet/archive/master.zip', 'fabnet_package_files.lst')
+
+        check_deps({GENTOO: ('pymongo', 'openssl', 'paramiko'), \
+                RHEL: ('openssl', 'python-paramiko'), \
+                DEBIAN: ('openssl', 'python-crypto')})
+    except Exception, err:
+        print (err)
+        sys.exit(1)
+
+    prepare_install('/opt/blik/fabnet', '/opt/blik/fabnet/mgmt_package_files.lst')
+
+    setup(
+        name = "fabnet-mgmt",
+        version = get_cur_ver(),
+        author = "Fabregas",
+        author_email = "kksstt@gmail.com",
+        description = ("Management core for fabnet network."),
+        license = "CC BY-NC",
+        url = "https://github.com/fabregas/fabnet_mgmt/wiki",
+        packages=['fabnet_mgmt', 'fabnet_ca'],
+        data_files=[('', ['VERSION'])],        
+        scripts=get_all('./bin'),
+        long_description=read('README'),
+    )
+    setup_user()
+    update_user_profile()
